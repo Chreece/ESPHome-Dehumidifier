bde149b7329825d7450d47c77cec2f57b5875123
Christos Baltatzidis <68458228+Chreece@users.noreply.github.com>
2025-10-19 16:42:26 +0200

    ok?

--- components/midea_dehum/datetime.py
+++ components/midea_dehum/datetime.py
@@ -1,14 +1,14 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import datetime
-from esphome.const import CONF_ID, ICON_TIMER
+from esphome.const import CONF_ID
 from . import midea_dehum_ns, CONF_MIDEA_DEHUM_ID
 
 cg.add_define("USE_MIDEA_DEHUM_DATETIME")
 
 MideaDehum = midea_dehum_ns.class_("MideaDehumComponent", cg.Component)
 
-# ✅ Use datetime.datetime_ns.Datetime for newer ESPHome versions
+# ✅ Works for ESPHome 2025.x (new datetime system)
 MideaTriggerDatetime = midea_dehum_ns.class_(
     "MideaTriggerDatetime", datetime.datetime_ns.class_("Datetime"), cg.Component
 )
@@ -17,10 +17,7 @@ CONF_TRIGGER_DATETIME = "trigger_datetime"
 
 CONFIG_SCHEMA = cv.Schema({
     cv.Required(CONF_MIDEA_DEHUM_ID): cv.use_id(MideaDehum),
-    cv.Optional(CONF_TRIGGER_DATETIME): datetime.datetime_schema(
-        MideaTriggerDatetime,
-        icon=ICON_TIMER,
-    ),
+    cv.Optional(CONF_TRIGGER_DATETIME): datetime.datetime_schema(MideaTriggerDatetime),
 })
 
 async def to_code(config):
