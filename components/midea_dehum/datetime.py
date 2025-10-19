4c6acef853911001d2716b19e5f655455c6c7b57
Christos Baltatzidis <68458228+Chreece@users.noreply.github.com>
2025-10-19 17:23:23 +0200

    i hate you chatgpt

--- components/midea_dehum/datetime.py
+++ components/midea_dehum/datetime.py
@@ -1,13 +1,13 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import datetime
+from esphome.components import datetime as dt
 from . import midea_dehum_ns, CONF_MIDEA_DEHUM_ID
 
 CONF_TRIGGER_DATETIME = "trigger_datetime"
 
 MideaTriggerDatetime = midea_dehum_ns.class_(
     "MideaTriggerDatetime",
-    datetime.DateTimeEntity,
+    dt.DateTimeEntity,
     cg.Component,
 )
 
@@ -15,12 +15,13 @@ MideaDehum = midea_dehum_ns.class_("MideaDehumComponent", cg.Component)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.Required(CONF_MIDEA_DEHUM_ID): cv.use_id(MideaDehum),
-    cv.Optional(CONF_TRIGGER_DATETIME): datetime.datetime_schema(MideaTriggerDatetime),
+    cv.Optional(CONF_TRIGGER_DATETIME): cv.use_id(dt.DateTimeEntity),
 })
 
 async def to_code(config):
     parent = await cg.get_variable(config[CONF_MIDEA_DEHUM_ID])
+    # ...your existing to_code...
     if CONF_TRIGGER_DATETIME in config:
+        dt_ent = await cg.get_variable(config[CONF_TRIGGER_DATETIME])
         cg.add_define("USE_MIDEA_DEHUM_DATETIME")
-        dt = await datetime.new_datetime(config[CONF_TRIGGER_DATETIME])
-        cg.add(parent.set_trigger_datetime(dt))
+        cg.add(parent.set_trigger_datetime(dt_ent))
