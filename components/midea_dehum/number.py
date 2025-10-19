a1adb97229b6626b0b84fe8813c17d97cda6b040
Christos Baltatzidis <68458228+Chreece@users.noreply.github.com>
2025-10-19 13:39:36 +0200

    unit_hour

--- components/midea_dehum/number.py
+++ components/midea_dehum/number.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import number
-from esphome.const import CONF_ID, UNIT_HOURS, ICON_TIMER_OUTLINE
+from esphome.const import CONF_ID, UNIT_HOUR, ICON_TIMER_OUTLINE
 from . import midea_dehum_ns, CONF_MIDEA_DEHUM_ID
 
 cg.add_define("USE_MIDEA_DEHUM_NUMBER")
@@ -15,7 +15,7 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Required(CONF_MIDEA_DEHUM_ID): cv.use_id(MideaDehum),
     cv.Optional(CONF_TIMER): number.number_schema(
         MideaTimerNumber,
-        unit_of_measurement=UNIT_HOURS,
+        unit_of_measurement=UNIT_HOUR,
         icon=ICON_TIMER_OUTLINE,
         min_value=0,
         max_value=24,
