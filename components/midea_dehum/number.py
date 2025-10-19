42d90857257c94bb0dc20d36c2e7848ae96c20f2
Christos Baltatzidis <68458228+Chreece@users.noreply.github.com>
2025-10-19 14:00:57 +0200

    number

--- components/midea_dehum/number.py
+++ components/midea_dehum/number.py
@@ -27,3 +27,6 @@ async def to_code(config):
         cg.add_define("USE_MIDEA_DEHUM_TIMER")
         n = await number.new_number(config[CONF_TIMER])
         cg.add(parent.set_timer_number(n))
+        cg.add(parent.set_min_value(0.5))
+        cg.add(parent.set_max_value(24))
+        cg.add(parent.set_step(0.5))
