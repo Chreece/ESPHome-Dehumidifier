ad3b189964055333e56101886418058ac36f2ce9
Chris Chreece <68458228+Chreece@users.noreply.github.com>
2025-10-21 10:42:01 +0200

    Add device information variables to midea_dehum.h
    Added appliance type, protocol version, and device info flag.

--- components/midea_dehum/midea_dehum.h
+++ components/midea_dehum/midea_dehum.h
@@ -217,6 +217,10 @@ class MideaDehumComponent : public climate::Climate,
   void sendQueuedPacket();
   void processPacket(uint8_t *data, size_t len);
 
+  uint8_t appliance_type_ = 0x00;
+  uint8_t protocol_version_ = 0x00;
+  bool device_info_known_ = false;
+
   uart::UARTComponent *uart_{nullptr};
   uint32_t status_poll_interval_{30000};
 
