2ac88b0036e97dcff352b9322697cfd4d5de441f
Chris Chreece <68458228+Chreece@users.noreply.github.com>
2025-10-23 09:51:43 +0200

    Update text_sensor.py

--- components/midea_dehum/text_sensor.py
+++ components/midea_dehum/text_sensor.py
@@ -16,16 +16,18 @@ CONF_CAPABILITIES = "capabilities"
 CONFIG_SCHEMA = cv.Schema({
     cv.Required(CONF_MIDEA_DEHUM_ID): cv.use_id(MideaDehum),
     cv.Optional(CONF_CAPABILITIES): text_sensor.text_sensor_schema(
+        MideaCapabilitiesTextSensor,
         icon="mdi:chip",
-        entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
+        entity_category="diagnostic",
     ),
 })
 
 async def to_code(config):
     parent = await cg.get_variable(config[CONF_MIDEA_DEHUM_ID])
+
     if CONF_CAPABILITIES in config:
         cg.add_define("USE_MIDEA_DEHUM_CAPABILITIES")
-        sens = cg.new_Pvariable(config[CONF_CAPABILITIES][CONF_ID], MideaCapabilitiesTextSensor)
+        sens = cg.new_Pvariable(config[CONF_CAPABILITIES][CONF_ID])
         await text_sensor.register_text_sensor(sens, config[CONF_CAPABILITIES])
         cg.add(sens.set_parent(parent))
         cg.add(parent.set_capabilities_text_sensor(sens))
