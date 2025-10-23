4f31d1660465c72235d3a1e65833a90909d8ef94
Chris Chreece <68458228+Chreece@users.noreply.github.com>
2025-10-23 09:46:14 +0200

    Update text_sensor.py

--- components/midea_dehum/text_sensor.py
+++ components/midea_dehum/text_sensor.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import text_sensor
-from esphome.const import ENTITY_CATEGORY_DIAGNOSTIC
+from esphome.const import CONF_ID, ENTITY_CATEGORY_DIAGNOSTIC
 from . import midea_dehum_ns, CONF_MIDEA_DEHUM_ID
 
 cg.add_define("USE_MIDEA_DEHUM_TEXT")
