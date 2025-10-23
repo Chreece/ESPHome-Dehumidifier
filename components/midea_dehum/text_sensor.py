6529b6ce5a0e3965c30f9b3bf7bcd7c5bdb13735
Chris Chreece <68458228+Chreece@users.noreply.github.com>
2025-10-23 08:31:36 +0200

    Create text_sensor.py

---
+++ components/midea_dehum/text_sensor.py
@@ -0,0 +1,30 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import text_sensor
+from esphome.const import ICON_LIST, ENTITY_CATEGORY_DIAGNOSTIC
+from . import midea_dehum_ns, CONF_MIDEA_DEHUM_ID
+
+cg.add_define("USE_MIDEA_DEHUM_TEXT")
+
+MideaDehum = midea_dehum_ns.class_("MideaDehumComponent", cg.Component)
+MideaCapabilitiesTextSensor = midea_dehum_ns.class_(
+    "MideaCapabilitiesTextSensor", text_sensor.TextSensor, cg.Component
+)
+
+CONF_CAPABILITIES = "capabilities"
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.Required(CONF_MIDEA_DEHUM_ID): cv.use_id(MideaDehum),
+    cv.Optional(CONF_CAPABILITIES): text_sensor.text_sensor_schema(
+        icon=ICON_LIST,
+        entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
+    ),
+})
+
+async def to_code(config):
+    parent = await cg.get_variable(config[CONF_MIDEA_DEHUM_ID])
+
+    if CONF_CAPABILITIES in config:
+        cg.add_define("USE_MIDEA_DEHUM_CAPABILITIES")
+        sens = await text_sensor.new_text_sensor(config[CONF_CAPABILITIES])
+        cg.add(parent.set_capabilities_text_sensor(sens))
