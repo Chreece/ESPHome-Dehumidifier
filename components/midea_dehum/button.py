4ffa312a1f4bad1e242a12992d8ebb8781d836b9
Christos Baltatzidis <68458228+Chreece@users.noreply.github.com>
2025-10-25 11:23:58 +0200

    moved button

--- components/midea_dehum/button.py
+++ components/midea_dehum/button.py
@@ -2,7 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import button
 from esphome.const import ENTITY_CATEGORY_CONFIG
-from . import midea_dehum_ns, CONF_MIDEA_DEHUM_ID
+from .. import midea_dehum_ns, CONF_MIDEA_DEHUM_ID
 
 cg.add_define("USE_MIDEA_DEHUM_BUTTON")
 
