166bcd0d87f69f0c831df4df657bf3c64a64c6d7
Christos Baltatzidis <68458228+Chreece@users.noreply.github.com>
2025-10-25 11:27:39 +0200

    added class

--- components/midea_dehum/button.py
+++ components/midea_dehum/button.py
@@ -14,6 +14,7 @@ CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(MideaDehum),
     cv.Required(CONF_MIDEA_DEHUM_ID): cv.use_id(MideaDehum),
     cv.Optional(CONF_FILTER_CLEANED): button.button_schema(
+        MideaDehum,
         icon="mdi:broom",
         entity_category=ENTITY_CATEGORY_CONFIG,
     ),
