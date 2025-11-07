38a86b58d190d468a8e4cafcfdcfe9e0939c200c
Christos Baltatzidis <68458228+Chreece@users.noreply.github.com>
2025-11-07 18:06:29 +0100

    error sensor fix

--- components/midea_dehum/midea_dehum.cpp
+++ components/midea_dehum/midea_dehum.cpp
@@ -864,9 +864,9 @@ void MideaDehumComponent::parseState() {
   }
 
 #if defined(USE_MIDEA_DEHUM_ERROR) || defined(USE_MIDEA_DEHUM_BUCKET)
-    if (this->error_sensor_ && (first_run || this->error_state_ != new_error)) {
+    if (first_run || this->error_state_ != new_error) {
       this->error_state_ = new_error;
-      this->error_sensor_->publish_state(this->error_state_);
+      if(this->error_sensor_) {this->error_sensor_->publish_state(this->error_state_);}
     }
 #endif
 
